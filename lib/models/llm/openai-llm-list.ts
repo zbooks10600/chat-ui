ea2cf68807221c455f5d5809cfd2587167bb4160
Meet Patel <113581962+meetpateltech@users.noreply.github.com>
2024-07-18 22:56:48 +0530

    added openai gpt4o mini model

--- lib/models/llm/openai-llm-list.ts
+++ lib/models/llm/openai-llm-list.ts
@@ -3,6 +3,22 @@ import { LLM } from "@/types"
 const OPENAI_PLATORM_LINK = "https://platform.openai.com/docs/overview"
 
 // OpenAI Models (UPDATED 1/25/24) -----------------------------
+
+const GPT4oMini: LLM = {
+  modelId: "gpt-4o-mini",
+  modelName: "GPT-4o Mini",
+  provider: "openai",
+  hostedId: "gpt-4o-mini",
+  platformLink: OPENAI_PLATORM_LINK,
+  imageInput: true,
+  pricing: {
+    currency: "USD",
+    unit: "1M tokens",
+    inputCost: 0.15,
+    outputCost: 0.6
+  }
+}
+
 const GPT4o: LLM = {
   modelId: "gpt-4o",
   modelName: "GPT-4o",
