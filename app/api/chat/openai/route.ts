f1f45a2d51fb61d67483b782ceb918f36787a2dc
Meet Patel <113581962+meetpateltech@users.noreply.github.com>
2024-07-19 08:53:22 +0530

    fix output token size

--- app/api/chat/openai/route.ts
+++ app/api/chat/openai/route.ts
@@ -30,10 +30,11 @@ export async function POST(request: Request) {
       temperature: chatSettings.temperature,
       max_tokens:
         chatSettings.model === "gpt-4-vision-preview" ||
-        chatSettings.model === "gpt-4o" ||
-        chatSettings.model === "gpt-4o-mini"
+        chatSettings.model === "gpt-4o"
           ? 4096
-          : null, // TODO: Fix
+          : chatSettings.model === "gpt-4o-mini"
+          ? 16383
+          : null, 
       stream: true
     })
 
