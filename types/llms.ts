f5da694467487c5a2530f949f670094aabf3cac3
Santhosh Sundararaman <santhoshsram@gmail.com>
2024-08-25 15:10:49 +0530

    add gpt4o-mini

--- types/llms.ts
+++ types/llms.ts
@@ -10,6 +10,7 @@ export type LLMID =
 
 // OpenAI Models (UPDATED 5/13/24)
 export type OpenAILLMID =
+  | "gpt-4o-mini" // GPT-4o Mini
   | "gpt-4o" // GPT-4o
   | "gpt-4-turbo-preview" // GPT-4 Turbo
   | "gpt-4-vision-preview" // GPT-4 Vision
